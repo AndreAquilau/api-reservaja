ec147224607c2e3480fd546f72859b5ac7d9c979
André Da Silva <Andre.Aquilau@outlook.com>
2021-05-18 21:38:16 -0400

    fix: app

--- src/services/app.ts
+++ src/services/app.ts
@@ -1,8 +1,26 @@
-import { Router } from 'express';
-import VagaController from '@controllers/VagaController';
+import express, { Application } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
+import routes from '@routes/index';
 
-const router = Router();
+class App {
+  app: Application;
 
-router.get('/', VagaController.verificar);
+  constructor() {
+    this.app = express();
+    this.loadDependencies();
+  }
 
-export default router;
+  loadDependencies() {
+    this.app.use(morgan('dev'));
+    this.app.use(helmet());
+    this.app.use(urlencoded({ extended: true }));
+    this.app.use(json());
+    this.app.use(cors());
+    this.app.use(routes);
+  }
+}
+
+export default new App().app;
